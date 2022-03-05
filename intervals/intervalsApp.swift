b9fd9a9bb6c31637856eed216054f282a812dfbb
Aydin Metel <aydin.metel@gmail.com>
2022-03-05 13:45:57 -0500

    Initial Commit

---
+++ intervals/intervalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  intervalsApp.swift
+//  intervals
+//
+//  Created by Aydin Metel on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct intervalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
